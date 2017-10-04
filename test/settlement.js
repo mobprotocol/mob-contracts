21846309628103219355eea0d467805522bbbf14
Sean Pollock <seanpollock3344@gmail.com>
2017-10-03 20:47:39 -0700

    fixing encoding scheme

--- test/settlement.js
+++ test/settlement.js
@@ -6,22 +6,22 @@ const users = require('../conf/users.json')
 contract('Settlement', (accounts) => {
   it ('Verify js and solidity sha3 conformity :)', async () => {
     try {
-      const settlement = await getContract()
+      const settlement = await Settlement.deployed()
       const hash1 = sha3('hello world').toString('hex').replace(/^/, '0x')
       const hash2 = await settlement.checkHash.call()
       assert.equal(hash1, hash2)
     } catch (err) {
-      new Error('### error in test1', err)
+      console.log('### error in test1', err)
     }
   })
 
   it ('Should verify signature', async () => {
     try {
-      const settlement = await getContract()
-      console.log('users', users)
-      const signature = ecsign
+      const settlement = await Settlement.deployed()
+      const signature = ecsign(hash1, new Buffer(users[0].privKey.substring(2), 'hex'))
+      console.log('signature', signature)
     } catch (err) {
-      new Error('### error in test2', err)
+      console.log('### error in test2', err)
     }
   })
 })
