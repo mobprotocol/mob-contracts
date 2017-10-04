215f7e46f3bba3f7381d2f055a21bab3aa052957
Sean Pollock <seanpollock3344@gmail.com>
2017-10-03 20:28:20 -0700

    settlement test working

--- test/settlement.js
+++ test/settlement.js
@@ -1,9 +1,27 @@
 const Settlement = artifacts.require('./Settlement')
 const { sha3, ecsign } = require('ethereumjs-util')
+const { getContract } = require('./utils')
+const users = require('../conf/users.json')
 
 contract('Settlement', (accounts) => {
-  it ('Should verify signer', () => {
-    const hash = sha3('hello world').toString('hex').replace(/^/, '0x')
-    console.log('hash', hash)
+  it ('Verify js and solidity sha3 conformity :)', async () => {
+    try {
+      const settlement = await getContract()
+      const hash1 = sha3('hello world').toString('hex').replace(/^/, '0x')
+      const hash2 = await settlement.checkHash.call()
+      assert.equal(hash1, hash2)
+    } catch (err) {
+      new Error('### error in test1', err)
+    }
+  })
+
+  it ('Should verify signature', async () => {
+    try {
+      const settlement = await getContract()
+      console.log('users', users)
+      const signature = ecsign
+    } catch (err) {
+      new Error('### error in test2', err)
+    }
   })
 })
