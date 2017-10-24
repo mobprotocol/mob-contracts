40109b1e81d228e1f1a1b562cf3154e833f47f60
Sean Pollock <seanpollock3344@gmail.com>
2017-10-24 14:32:11 -0400

    signing w/ js tools

--- test/settlement.js
+++ test/settlement.js
@@ -6,7 +6,6 @@ const users = require('../conf/users.json')
 contract('Settlement', (accounts) => {
   it ('Verify js and solidity sha3 conformity :)', async () => {
     try {
-      const settlement = await Settlement.deployed()
       const hash1 = sha3('hello world').toString('hex').replace(/^/, '0x')
       const hash2 = await settlement.checkHash.call()
       assert.equal(hash1, hash2)
@@ -20,11 +19,12 @@ contract('Settlement', (accounts) => {
       const settlement = await Settlement.deployed()
       const hash = sha3('hello world')
       const signature = ecsign(hash, new Buffer(users[0].privKey.substring(2), 'hex'))
-      const sig = {
-        v: signature.v,
-        r: signature.r.toString('hex'),
-        s: signature.s.toString('hex')
-      }
+      const sig = [
+        signature.v,
+        signature.r.toString('hex'),
+        signature.s.toString('hex')
+      ]
+      
     } catch (err) {
       console.log('### error in test2', err)
     }
