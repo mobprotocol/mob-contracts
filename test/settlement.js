8e682855956b7904d3e577934a8b69d85f06faa0
Sean Pollock <seanpollock3344@gmail.com>
2017-10-25 10:45:10 -0400

    wip. debugging ecsign in js

--- test/settlement.js
+++ test/settlement.js
@@ -1,14 +1,29 @@
 const Settlement = artifacts.require('./Settlement')
-const { sha3, ecsign } = require('ethereumjs-util')
+const { sha3, ecsign, ecrecover, hashPersonalMessage } = require('ethereumjs-util')
 const { getContract } = require('./utils')
 const users = require('../conf/users.json')
+const Web3 = require('web3')
+const soliditysha3 = require('solidity-sha3').default
+const web3 = new Web3()
+const { eth } = web3
 
 contract('Settlement', (accounts) => {
   it ('Verify js and solidity sha3 conformity :)', async () => {
     try {
-      const hash1 = sha3('hello world').toString('hex').replace(/^/, '0x')
-      const hash2 = await settlement.checkHash.call()
-      assert.equal(hash1, hash2)
+      const hash = sha3('hello world')
+      const msgHash = hashPersonalMessage(hash)
+      const settlement = await Settlement.deployed()
+      const hash2 = await settlement.verifyMsg.call()
+      console.log('hash1', hash.toString('hex'))
+      console.log('hash2', hash2)
+      const msgHash2 = await settlement.verifyHash.call(hash)
+      const msgHash3 = soliditysha3("\x19Ethereum Signed Message:\n32", hash)
+      console.log('msgHash', msgHash.toString('hex'))
+      console.log('msgHash2', msgHash2)
+      console.log('msgHash3', msgHash3)
+      // const hash2 = await settlement.checkHash.call()
+      // assert.equal(hash1, hash2)
+
     } catch (err) {
       console.log('### error in test1', err)
     }
@@ -17,14 +32,19 @@ contract('Settlement', (accounts) => {
   it ('Should verify signature', async () => {
     try {
       const settlement = await Settlement.deployed()
-      const hash = sha3('hello world')
+      const hash = hashPersonalMessage(Buffer.from('hello world', 'utf8'))
+      console.log('hash', hash)
       const signature = ecsign(hash, new Buffer(users[0].privKey.substring(2), 'hex'))
       const sig = [
         signature.v,
         signature.r.toString('hex'),
         signature.s.toString('hex')
       ]
-      
+      console.log('hash', hash.toString('hex').replace(/^/, '0x'))
+      console.log('sig', sig)
+      const address = await settlement.verifySignature.call(hash, sig)
+      console.log('address', address)
+      console.log('signer', users[0].pubAddress)
     } catch (err) {
       console.log('### error in test2', err)
     }
