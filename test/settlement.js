b0e4cde200c7fa41a738ad158c2e748a57be1d3e
Sean Pollock <seanpollock3344@gmail.com>
2017-10-25 12:51:10 -0400

    sig tests passing

--- test/settlement.js
+++ test/settlement.js
@@ -12,22 +12,18 @@ contract('Settlement', (accounts) => {
     try {
       const settlement = await Settlement.deployed()
       const hash = sha3('hello world')
-      console.log('hash here', hash.toString('hex').replace(/^/, '0x'))
       const msgHash = await settlement.verifyHash.call(hash.toString('hex').replace(/^/, '0x'))
       const msgHash2 = hashPersonalMessage(hash).toString('hex').replace(/^/, '0x')
-
-      console.log('msgHash', msgHash)
-      console.log('msgHash2', msgHash2)
+      assert.equal(msgHash, msgHash2)
     } catch (err) {
       console.log('### error in test1', err)
     }
   })
 
-  it ('Should verify signature w/ ethereumjs-util', async () => {
+  it ('Should verify signature with simple hello world message', async () => {
     try {
       const settlement = await Settlement.deployed()
       const hash = sha3('hello world')
-      console.log('hash check', hash)
       const msgHash = hashPersonalMessage(hash)
       const signature = ecsign(msgHash, new Buffer(users[0].secretKey.substring(2), 'hex'))
       const sig = [
@@ -35,16 +31,13 @@ contract('Settlement', (accounts) => {
         '0x' + signature.r.toString('hex'),
         '0x' + signature.s.toString('hex')
       ]
-      console.log('msgHash', msgHash.toString('hex').replace(/^/, '0x'))
-      console.log('sig', sig)
       const address = await settlement.verifySignature.call(
         hash.toString('hex').replace(/^/, '0x'),
         sig[0],
         sig[1],
         sig[2]
       )
-      console.log('signer', users[0].pubAddress)
-      console.log('address', address)
+      assert.equal(users[0].pubAddress, address)
     } catch (err) {
       console.log('### error in test2', err)
     }
@@ -58,14 +51,8 @@ contract('Settlement', (accounts) => {
       const r = "0x"+signature.substr(2, 64)
       const s = "0x"+signature.substr(66, 64)
       const v = 27 + Number(signature.substr(130, 2))
-      const sig = [v, r, s]
-      console.log('v', v)
-      console.log('r', r)
-      console.log('s', s)
-      console.log('hash', hash)
       const address = await settlement.verifySignature.call(hash, v, r, s)
-      console.log('actual signer', users[0].pubAddress)
-      console.log('recovered signer', address)
+      assert.equal(users[0].pubAddress, address)
     } catch (err) {
       console.log(err)
     }
