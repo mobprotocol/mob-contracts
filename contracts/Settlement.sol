c328f6b907e0238255e695d7386e5f976809dc4e
Sean Pollock <seanpollock3344@gmail.com>
2017-10-25 12:51:42 -0400

    test fns

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -1,10 +1,16 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.7;
 
 contract Settlement {
-  function verifySignature(bytes32 _msg, bytes32[3] _sig) returns (address) {
-    bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-    bytes32 msgHash = sha3(prefix, _msg);
-    address signer = ecrecover(msgHash, uint8(_sig[2]), _sig[0], _sig[1]);
-    return signer;
-  }
+    function verifyMsg() returns (bytes32) {
+        return sha3('hello world');
+    }
+    function verifyHash(bytes32 _msg) returns (bytes32) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        return sha3(prefix, _msg);
+    }
+    function verifySignature(bytes32 _msg, uint8 v, bytes32 r, bytes32 s) returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 msgHash = sha3(prefix, _msg);
+        return ecrecover(msgHash, v, r, s);
+    }
 }
