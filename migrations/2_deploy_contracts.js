e81a91b0a53ad1976c97b084cf0b0cedd53292ef
Sean Pollock <seanpollock3344@gmail.com>
2017-10-25 16:11:33 -0400

    adding token creation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const Settlement = artifacts.require('./Settlement.sol')
+const Token = artifacts.require('./Token.sol')
+const Tokens = require('../conf/tokens.json')
 
 module.exports = function(deployer) {
+  deployer.deploy(Token, Tokens[0].name, Tokens[0].symbol, Tokens[0].decimals, Tokens[0].supply)
+  deployer.deploy(Token, Tokens[1].name, Tokens[1].symbol, Tokens[1].decimals, Tokens[1].supply)
   deployer.deploy(Settlement)
 }
