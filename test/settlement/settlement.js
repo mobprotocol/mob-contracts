bb7338f837945d6249ea72e0cb6f218812534ca5
Sean Pollock <seanpollock3344@gmail.com>
2017-10-26 11:58:52 -0400

    before hook creates token contracts

---
+++ test/settlement/settlement.js
@@ -0,0 +1,27 @@
+const Settlement = artifacts.require('./Settlement')
+const Token = artifacts.require('./Token')
+const Tokens = require('../../conf/tokens.json')
+const BN = require('bn.js')
+
+contract('Settlement', accts => {
+  before(async () => {
+    console.log('### before')
+    const settlement = await Settlement.deployed()
+    const TokenA = Token.new(Tokens[0].name, Tokens[0].symbol, Tokens[0].decimals, Tokens[0].supply)
+    const TokenB = Token.new(Tokens[1].name, Tokens[1].symbol, Tokens[1].decimals, Tokens[1].supply)
+    console.log('tokenA', TokenA.address)
+  })
+
+  it('Should submit two signed orders to atomic swap', async () => {
+    try  {
+
+      const order1 = {
+        // price = price / 1000
+        price: new BN(550, 10),
+        quantity: new BN(50)
+      }
+    } catch (err) {
+      console.log('### error in atomic swap test 1', err)
+    }
+  })
+})
