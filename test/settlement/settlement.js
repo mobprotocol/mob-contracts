b69b5f1f4b3ff1d7b8fb7e41da2a9868e83d4b47
Sean Pollock <seanpollock3344@gmail.com>
2017-10-26 12:00:36 -0400

    small fix

--- test/settlement/settlement.js
+++ test/settlement/settlement.js
@@ -7,9 +7,8 @@ contract('Settlement', accts => {
   before(async () => {
     console.log('### before')
     const settlement = await Settlement.deployed()
-    const TokenA = Token.new(Tokens[0].name, Tokens[0].symbol, Tokens[0].decimals, Tokens[0].supply)
-    const TokenB = Token.new(Tokens[1].name, Tokens[1].symbol, Tokens[1].decimals, Tokens[1].supply)
-    console.log('tokenA', TokenA.address)
+    const TokenA = await Token.new(Tokens[0].name, Tokens[0].symbol, Tokens[0].decimals, Tokens[0].supply)
+    const TokenB = await Token.new(Tokens[1].name, Tokens[1].symbol, Tokens[1].decimals, Tokens[1].supply)
   })
 
   it('Should submit two signed orders to atomic swap', async () => {
