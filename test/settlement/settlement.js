fd578c50ccab50944e9f7f1dfa1dc782c8ba686a
Sean Pollock <seanpollock3344@gmail.com>
2017-10-26 12:38:42 -0400

    two mock orders in the market

--- test/settlement/settlement.js
+++ test/settlement/settlement.js
@@ -5,21 +5,31 @@ const BN = require('bn.js')
 const { calcPermID } = require('./utils')
 
 contract('Settlement', accts => {
+  let settlement
+  let TokenA
+  let TokenB
+  let permutationID
   before(async () => {
     console.log('### before')
-    const settlement = await Settlement.deployed()
-    const TokenA = await Token.new(Tokens[0].name, Tokens[0].symbol, Tokens[0].decimals, Tokens[0].supply)
-    const TokenB = await Token.new(Tokens[1].name, Tokens[1].symbol, Tokens[1].decimals, Tokens[1].supply)
-    const permutationID = await calcPermID(TokenA.address, TokenB.address)
+    settlement = await Settlement.deployed()
+    TokenA = await Token.new(Tokens[0].name, Tokens[0].symbol, Tokens[0].decimals, Tokens[0].supply)
+    TokenB = await Token.new(Tokens[1].name, Tokens[1].symbol, Tokens[1].decimals, Tokens[1].supply)
+    permutationID = await calcPermID(TokenA.address, TokenB.address)
   })
 
   it('Should submit two signed orders to atomic swap', async () => {
     try  {
-
       const order1 = {
-        // price = price / 1000
         price: new BN(550, 10),
-        quantity: new BN(50)
+        quantity: new BN(50),
+        sell: TokenA.address,
+        permutationID
+      }
+      const order2 = {
+        price: new BN(1500, 10),
+        quanity: new BN(100),
+        sell: TokenB.address,
+        permutationID
       }
     } catch (err) {
       console.log('### error in atomic swap test 1', err)
